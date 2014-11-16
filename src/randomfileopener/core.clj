b6f25686bf6d8bf117aea61963702ddf598cc61a
Anurag Joshi <aj100@users.noreply.github.com>
2014-11-16 10:52:59 -0500

    Finished adding basic functionality

--- src/randomfileopener/core.clj
+++ src/randomfileopener/core.clj
@@ -13,33 +13,22 @@
 
 (defn open-random-file [directory is-nested]
   (letfn [(get-files [dir nested]
-                     (FileUtils/listFiles (io/file dir) nil (= nested "nested")))])
-  (let [files (get-files directory is-nested)]
-    (if (empty? files)
-      (println "No files found in the given directory.")
-      (let [sfiles (shuffle files)]
-        (map println sfiles)))))
+                     (FileUtils/listFiles (io/file dir) nil (= nested "nested")))
+          (open-file [desktop file]
+                     (.open desktop file))]
+    (let [files (get-files directory is-nested)]
+      (if (empty? files)
+        (println "No files found in the given directory.")
+        (let [sfiles  (shuffle files)
+              desktop (java.awt.Desktop/getDesktop) ]
+          (doseq [f sfiles]
+            (print (.getAbsolutePath f))
+            (flush)
+            (open-file desktop f)
+            (read-line))
+          (println "No more files to open."))))))
 
 (defn -main [& args]
   (if-not (verify? args)
     (println "usage: lein run \"C:/YourDirectoryPath\" [nested]")
     (open-random-file (first args) (second args))))
-
-(-main "C:/Users/Anurag/Desktop/TestFolder" "nested")
-
-;; (def files
-;;   (.listFiles (io/file "C:/Users/Anurag/Documents/Projects/Clojure")))
-
-
-;; (map println files)
-
-
-;;(.open (java.awt.Desktop/getDesktop) (io/file "C:/Users/Anurag/Desktop/082114_1217_goodguy1.png"))
-
-
-;(def directory (clojure.java.io/file "C:/Users/Anurag/Documents/Projects/Clojure"))
-;(def files (file-seq directory))
-;(take 10 files)
-
-
-;;

cb841ef7367d8f60c222a4326ea9e778555d1de0
Anurag Joshi <aj100@users.noreply.github.com>
2014-11-16 09:01:37 -0500

    Listing files w/ Apache FileUtils and randomizing list.

--- src/randomfileopener/core.clj
+++ src/randomfileopener/core.clj
@@ -1,21 +1,31 @@
 (ns randomfileopener.core
-  (:require [clojure.tools.cli :refer [parse-opts]])
-  (:import (java.awt Desktop))
+  (:require [clojure.java.io :as io])
+  (:import (java.awt Desktop)
+           (org.apache.commons.io FileUtils))
   (:gen-class :main true))
 
-
-(defn -main [& args]
-  (if-not (verify-params args)
-    (println "usage: lein run \"C:/YourDirectoryPath\" [nested]")
-    (println "valid")))
-
-(defn verify-params [args]
+(defn verify? [args]
   "There are either one or two arguments and the second argument has to be the word 'nested'"
   (let [args-count (count (into [] args))]
-    (or (= args-count 2) (= args-count 1))))
+    (or (= args-count 1)
+        (and (= args-count 2)
+             (= (second args) "nested")))))
 
+(defn open-random-file [directory is-nested]
+  (letfn [(get-files [dir nested]
+                     (FileUtils/listFiles (io/file dir) nil (= nested "nested")))])
+  (let [files (get-files directory is-nested)]
+    (if (empty? files)
+      (println "No files found in the given directory.")
+      (let [sfiles (shuffle files)]
+        (map println sfiles)))))
 
-(-main "a" "b" "c")
+(defn -main [& args]
+  (if-not (verify? args)
+    (println "usage: lein run \"C:/YourDirectoryPath\" [nested]")
+    (open-random-file (first args) (second args))))
+
+(-main "C:/Users/Anurag/Desktop/TestFolder" "nested")
 
 ;; (def files
 ;;   (.listFiles (io/file "C:/Users/Anurag/Documents/Projects/Clojure")))
